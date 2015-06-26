175f5321b131daae18f22205243dfecfa9105ab5
8bitgentleman <mainmoniker@googlemail.com>
2015-06-26 12:43:08 -0400

    Paintball

---
+++ src/items/weapons/paintball.lua
@@ -0,0 +1,12 @@
+return{
+  name = "paintball",
+  description = "Paintball",
+  type = "weapon",
+  subtype = "ammo",
+  damage = 2,
+  special_damage = 'paint= 1',
+  info = 'a set of 5 paintballs',
+  MAX_ITEMS = 99,
+  quantity = 5,
+  directory = 'weapons/',
+}
---
+++ src/nodes/projectiles/paintball.lua
@@ -0,0 +1,36 @@
+local game = require 'game'
+return{
+  name = 'paintball',
+  type = 'projectile',
+  friction = 1,
+  width = 6,
+  height = 6,
+  frameWidth = 6,
+  frameHeight = 6,
+  solid = true,
+  lift = game.gravity * 0.88,
+  playerCanPickUp = false,
+  enemyCanPickUp = false,
+  canPlayerStore = true,
+  usedAsAmmo = true,
+  throw_sound = 'arrow',
+  velocity = { x = -600, y = 0 }, --initial velocity
+  throwVelocityX = 600, 
+  throwVelocityY = -30,
+  thrown = false,
+  damage = 0,
+  special_damage = {paint = 1},
+  horizontalLimit = 800,
+  animations = {
+    default = {'once', {'1,1'},1},
+    thrown = {'once', {'1,1'}, 1},
+    finish = {'once', {'1,1'}, 1},
+  },
+  collide = function(node, dt, mtv_x, mtv_y,projectile)
+    if node.isPlayer then return end
+    if node.hurt then
+      node:hurt(projectile.damage, projectile.special_damage, 0)
+      projectile:die()
+    end
+  end,
+}
